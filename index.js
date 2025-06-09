95125da24b927fd43b29bd011b1fa761a774cc91
Shubham Singh <shubham@techtoabroad.com>
2025-06-09 13:18:02 +0530

    date feature added

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const timeElement = document.getElementById("time-element");
 const dateElement = document.getElementById("date-element");
 const amPmElement = document.getElementById("amPm-element");
-const dummyTime = "01";
 
-function updateTime() {
-  const now = new Date();
+function getDate(now) {
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = now.toLocaleDateString("en-US", options);
+  dateElement.innerText=formattedDate
+}
+
+function updateTime(now) {
+    console.log("now", now)
   const originalHours = now.getHours();
-  const hours = originalHours % 12 || 12;
+  const modifiedHours = originalHours % 12 || 12;
+  const hours = modifiedHours.toString().padStart(2, "0");
   const minutes = now.getMinutes().toString().padStart(2, "0");
   const seconds = now.getSeconds().toString().padStart(2, "0");
   const amPm = originalHours < 12 ? "am" : "pm";
-  console.log("hours", hours);
+
   timeElement.innerText = `${hours}:${minutes}:${seconds}`;
   amPmElement.innerText = amPm;
 }
 
+updateTime(new Date());
+
 setInterval(() => {
-  updateTime();
+  const now = new Date();
+  updateTime(now);
+  getDate(now);
 }, 1000);
